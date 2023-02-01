1d74513ccc21f0da5f9a88a9f1842b6c8831ce50
TripleYei <58819156+TripleYei@users.noreply.github.com>
2023-02-01 17:27:17 +0100

    Add files via upload

---
+++ netcat.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+cat << EOF
+
+ 
+ _____________________ 
+|  _________________  |
+| | BANNER GRABBING | |
+| |_________________| |
+|_____________________|
+                                                                                                  
+ 
+                                                                                                 
+Triple Yei
+
+EOF
+
+
+echo "Introduce el dominio"
+read dominio
+nc -v $dominio 80
---
+++ nmap.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+cat << EOF
+
+ 
+ _____________________ 
+|  _________________  |
+| | FINGERPRINTING | |
+| |_________________| |
+|_____________________|
+                                                                                                  
+ 
+                                                                                                 
+Triple Yei
+
+EOF
+
+
+echo "Introduce el dominio"
+read dominio
+nmap -O $dominio 
